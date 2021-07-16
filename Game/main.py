d11d793d54f245ab108a8042a34147be0773532e
Naman Biswajit <62687888+Naman-Biswajit@users.noreply.github.com>
2021-07-16 15:55:12 +0530

    Added functions to load images and displaying

--- Game/main.py
+++ Game/main.py
@@ -1,5 +1,5 @@
 import pygame
-# import os
+import os
 
 pygame.init()
 
@@ -7,17 +7,33 @@ width = 1000
 height = 600
 bg_color = (255, 255, 255)
 
-screen_1 = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Hang Man')
-screen_1.fill(bg_color)
 
 Ticks = 50
 clock = pygame.time.Clock()
 
 gameloop = True
 
+
+def load_imgs(folder: str):
+    imgs = []
+
+    for img in os.listdir(f'./{folder}'):
+        img_surf  = pygame.image.load(f'./{folder}/{img}')
+        imgs.append(img_surf)
+    
+    return imgs
+
+
+hang_surf  = load_imgs('HangPos')
+
 while gameloop:
+    step = 0 # value for Hanging Status
+
     clock.tick(Ticks)
+    screen.fill(bg_color)
+    screen.blit(hang_surf[step], (100, 100))
 
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
@@ -29,3 +45,5 @@ while gameloop:
         if event.type == pygame.MOUSEBUTTONDOWN:
             cords = pygame.mouse.get_pos()
             print(cords)
+
+    pygame.display.update()
